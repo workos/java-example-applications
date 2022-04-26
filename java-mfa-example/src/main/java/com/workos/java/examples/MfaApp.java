a88d3ab782a7475ee5659b72a239284612f85c15
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2022-04-26 17:47:58 -0600

    Add enroll_factor method and update template

--- java-mfa-example/src/main/java/com/workos/java/examples/MfaApp.java
+++ java-mfa-example/src/main/java/com/workos/java/examples/MfaApp.java
@@ -1,13 +1,19 @@
 package com.workos.java.examples;
 
 import com.workos.WorkOS;
+import com.workos.mfa.MfaApi;
+import com.workos.mfa.MfaApi.EnrollFactorOptions;
+import com.workos.mfa.models.Factor;
+
 import com.workos.sso.models.ProfileAndToken;
 import io.github.cdimascio.dotenv.Dotenv;
 import io.javalin.Javalin;
 import io.javalin.http.Context;
 import io.javalin.http.staticfiles.Location;
+import java.util.ArrayList;
 import java.util.Collections;
-
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MfaApp {
@@ -28,14 +34,36 @@ public class MfaApp {
     app.get("/login", this::login);
     app.get("/callback", this::callback);
     app.get("logout", this::logout);
-    app.get("/enroll_factor", this::enroll_factor);
+    app.post("/enroll_factor", this::enroll_factor);
   }
 
-
   public void enroll_factor(Context ctx) {
-    System.out.print("hit the method");
+    String phoneNumber = ctx.formParam("phone_number");
+    EnrollFactorOptions options = MfaApi.EnrollFactorOptions.builder()
+      .type("sms")
+      .phoneNumber(phoneNumber)
+      .build();
+    Factor factor = workos.mfa.enrollFactor(options);
 
-    ctx.redirect("/");
+    if(ctx.sessionAttribute("factorList") != null) {
+      System.out.print("hit not-null part");
+      Map<String, Factor> factorList = ctx.sessionAttribute("factorList");
+      factorList.put(factor.id, factor);
+      ctx.sessionAttribute("factorList", factorList);
+    } else {
+      System.out.print("hit null part");
+      Map<String, Factor> factorList = new HashMap<>();
+      factorList.put(factor.id, factor);
+
+      ctx.sessionAttribute("factorList", factorList);
+    }
+
+    Map<String, Factor> factorList = ctx.sessionAttribute("factorList");
+
+    System.out.println("factorlist before render");
+    System.out.println(factorList);
+
+    ctx.render("home.jte", factorList);
   }
 
   public void login(Context ctx) {
