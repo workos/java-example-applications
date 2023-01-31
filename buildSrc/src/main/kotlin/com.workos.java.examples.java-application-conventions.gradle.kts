611796b11cf580e426cdf2232a343fbf625d6dce
Adam Wolfman <adamwolfman@Adams-MBP-3.domain>
2023-01-31 14:38:22 -0700

    Update to latest workos SDK version

--- buildSrc/src/main/kotlin/com.workos.java.examples.java-application-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/com.workos.java.examples.java-application-conventions.gradle.kts
@@ -9,7 +9,7 @@ dependencies {
 
   implementation("gg.jte:jte:1.12.0")
 
-  implementation("com.workos:workos:2.9.0")
+  implementation("com.workos:workos:2.9.1")
 
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
 
